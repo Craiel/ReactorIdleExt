984c92b52a26bb5f7df6216877504a1377b2793b
Patrick Gustavel <craieldev@gmail.com>
2019-04-05 16:07:05 -0400

    added total research + money overview

--- Src/template/reactors.tpl.js
+++ Src/template/reactors.tpl.js
@@ -31,7 +31,7 @@ templates['reactors'] = template({"1":function(container,depth0,helpers,partials
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"reactors\">\r\n    <div class=\"header\">\r\n        You have <span class=\"money\">$<b id=\"money\" class=\"money\"></b></span> to spend. Isn't that nice.\r\n    </div>\r\n    <div class=\"help\">\r\n        Buy more land to create even bigger power empire. Go ahead, buy some!\r\n    </div>\r\n    <div class=\"selectButtons\">\r\n"
+  return "<div class=\"reactors\">\r\n    <div class=\"textLine\">\r\n        <span class=\"productionTitle\">Money:</span>\r\n        <span class=\"money have\" id=\"money\">0</span>\r\n        <span class=\"money production\" id=\"income\">0</span>\r\n    </div>\r\n\r\n    <div class=\"textLine\">\r\n        <span class=\"productionTitle\">Research:</span>\r\n        <span class=\"research have\" id=\"research\">0</span>\r\n        <span class=\"research production\" id=\"researchProduction\">0</span>\r\n    </div>\r\n    <div class=\"help\">\r\n        Buy more land to create even bigger power empire. Go ahead, buy some!\r\n    </div>\r\n    <div class=\"selectButtons\">\r\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.reactors : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "    </div>\r\n</div>\r\n";
 },"useData":true});
--- Src/ui/Reactors.js
+++ Src/ui/Reactors.js
@@ -4,8 +4,18 @@ RID.setModule("ui/Reactors", function() {
 
     var a = "worldsArea",
         n = function(t) {
-            this.game = t
-        }
+            this.game = t;
+            this.lastUpdateMoney = 0;
+            this.lastUpdateResearch = 0;
+            this.mps = {
+                v: 0,
+                t: 0
+            };
+            this.rps = {
+                    v: 0,
+                    t: 0
+                }
+        };
     return n.prototype.display = function(n) {
         var i = this
         this.container = n
@@ -39,7 +49,28 @@ RID.setModule("ui/Reactors", function() {
             $(".money[data-id=" + t.getMeta().id + "]").html(nfPlus(e.moneyProduction)), $(".research[data-id=" + t.getMeta().id + "]").html(nfPlus(e.researchProduction))
         })
     }, n.prototype.update = function() {
-        this.container.find("#money").html(nf(this.game.getMoney()))
+        var currentMoney = this.game.getMoney();
+        this.mps.t++;
+        this.mps.v += currentMoney - this.lastUpdateMoney;
+        this.lastUpdateMoney = currentMoney;
+        if(this.mps.t >= 40) {
+            this.container.find("#income").html("+" + nf(this.mps.v) + "/s");
+            this.mps.v = 0;
+            this.mps.t = 0;
+        }
+
+        var currentResearch = this.game.getResearchPoints();
+        this.rps.t++;
+        this.rps.v += currentResearch - this.lastUpdateResearch;
+        this.lastUpdateResearch = currentResearch;
+        if(this.rps.t >= 40) {
+            this.container.find("#researchProduction").html("+" + nf(this.rps.v) + "/s");
+            this.rps.v = 0;
+            this.rps.t = 0;
+        }
+
+        this.container.find("#money").html(nf(this.game.getMoney()));
+        this.container.find("#research").html(nf(this.game.getResearchPoints()));
         for (var t in this.game.getMeta().reactors) {
             var a = this.game.getMeta().reactors[t],
                 n = this.game.getReactor(a.id)
